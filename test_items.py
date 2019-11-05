a26d7aa1c5359fea3c9364046c0005d205b23508
Мокшин Сергей <52034863+SergeiMokshin@users.noreply.github.com>
2019-11-05 20:08:20 +0200

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -7,9 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_check_for_button(browser):
     browser.get(link)
-    time.sleep(30)
-    try:
-        button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-        print(f"button text = {button.text}")
-    except NoSuchElementException:
-        assert "button not found"
+    #time.sleep(30)
+    button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    print(f"button text = {button.text}")
+    assert button == button.text, "button is not found"
